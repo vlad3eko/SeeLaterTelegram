d3aae9ad14876331732134a1f5522810d03ff0f1
vlad3eko <vlad3eko@gmail.com>
2026-05-17 02:05:39 +0700

    delete get methods from movie.store add it on server/api/movies/
    changed SPA on SSR

---
+++ server/api/movies/index.get.ts
@@ -0,0 +1,21 @@
+import {serverSupabaseClient} from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+
+    const query = getQuery(event)
+
+
+    const sortBy =
+        typeof query.sortBy === 'string'
+            ? query.sortBy
+            : 'created_at'
+
+    const supabase = await serverSupabaseClient(event)
+
+    const {data, error} = await supabase
+        .from('movies')
+        .select()
+        .order(sortBy, {ascending: false})
+
+    return {data, error}
+})
