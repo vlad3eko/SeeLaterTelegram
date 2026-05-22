9a96fa56b8721d91e1dac83ef93c54901c8226ac
vlad3eko <vlad3eko@gmail.com>
2026-05-22 22:01:16 +0700

    Create Swiper Slide

--- app/components/ui/HorizontalScroller/index.vue
+++ app/components/ui/HorizontalScroller/index.vue
@@ -5,16 +5,20 @@
   </h1>
 
   <div class="border-b border-accent-foreground/20"/>
+    <div class="relative px-9">
   <ClientOnly>
-    <swiper-container ref="swiperRef" :init="false"
-                      class="overflow-hidden relative">
-      <swiper-slide v-for="item in items"
-                    :id="item.id"
-                    class="w-max md:h-max md:w-40 h-50">
-        <component :is="component" :item="item"/>
-      </swiper-slide>
-    </swiper-container>
+    <span class="material-symbols-outlined absolute top-1/2 -translate-y-1/2 left-0 select-none" @click="swiper.prev()">arrow_back_ios</span>
+      <swiper-container ref="swiperRef" :init="false"
+                        class="overflow-hidden relative ">
+        <swiper-slide v-for="item in items"
+                      :id="item.id"
+                      class="w-max md:h-max md:w-40 h-50">
+          <component :is="component" :item="item"/>
+        </swiper-slide>
+      </swiper-container>
+        <span class="material-symbols-outlined absolute top-1/2 -translate-y-1/2 right-0 select-none" @click="swiper.next()">arrow_forward_ios</span>
   </ClientOnly>
+    </div>
 
   <!-- TODO разобрать что это и как использовать -->
   <!--{{ credits.cast[0].known_for_department }}-->
--- app/components/widgets/web/person/PersonCard.vue
+++ app/components/widgets/web/person/PersonCard.vue
@@ -2,30 +2,30 @@
   <!-- CARD -->
   <NuxtLink
       class="relative block w-40 h-60 shrink-0 overflow-hidden rounded-xl cursor-pointer group">
+
     <NuxtImg
         :src="`https://image.tmdb.org/t/p/w300${item.profile_path}`"
-        class="absolute inset-0 w-full h-full object-cover z-10
+        class="absolute inset-0 w-full h-full object-cover
     transition duration-500 group-hover:scale-105"/>
     <div
-        class="absolute inset-0 z-20
+        class="absolute inset-0
                bg-gradient-to-t
                from-black/80
                via-black/20
                to-transparent"/>
     <span
         class="material-symbols-outlined
-               absolute top-2 right-2 z-30
+               absolute top-2 right-2
                transition select-none md-16">
         info
     </span>
     <p
         class="absolute bottom-3 left-1/2 -translate-x-1/2
-               z-30 w-full px-2
+               w-full px-2
                text-center text-sm text-white font-medium
                line-clamp-2">
       {{ item.name }}
     </p>
-
   </NuxtLink>
 </template>
 
