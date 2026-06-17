fd04a7c67a62c07853fdef84ce96e58b7a4e9caa
vlad3eko <vlad3eko@gmail.com>
2026-06-17 19:34:49 +0700

    edit slug "trailer"

---
+++ app/components/widgets/web/trailers/TrailerCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative"
+       @click.prevent="handleSelect(props.item.key)">
+    <iframe v-if="props.item.key"
+            class="w-full aspect-video md:h-[150px]"
+            :src="`https://www.youtube.com/embed/${props.item.key}`"
+            allowfullscreen/>
+    <span class="absolute top-0 right-0 h-full opacity-0 w-full cursor-pointer">
+    {{ props.item.name }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+interface Props {
+  item: any
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  select: [string]
+}>()
+
+const handleSelect = (id: string) => {
+  emit('select', id)
+}
+
+</script>
