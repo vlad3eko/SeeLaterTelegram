59871c78454fedf40d036f6a15645cc8c9417e4d
vlad3eko <vlad3eko@gmail.com>
2026-06-21 17:50:11 +0700

    edit server bot "fix bot life circle"

--- server/bot/actions/addMedia/saveMedia.ts
+++ server/bot/actions/addMedia/saveMedia.ts
@@ -5,6 +5,8 @@ export const saveMedia = async (ctx: any) => {
     const mediaId = Number(ctx.match[3])
     const mediaType = ctx.match[4]
 
+    await ctx.reply('Старт сохранения')
+
     const {success, error} = await $fetch('/api/bot/saveMediaBot', {
         method: 'POST',
         body: {
@@ -14,6 +16,8 @@ export const saveMedia = async (ctx: any) => {
             mediaType
         }
     })
+
+    await ctx.reply('Ентри сохранения')
 
 
     if (!success) {
