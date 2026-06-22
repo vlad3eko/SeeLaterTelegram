31f992821ef2f234c2420fc759617ba6f62e2b1f
vlad3eko <vlad3eko@gmail.com>
2026-06-22 21:56:13 +0700

    edit server bot "fix bot life circle"

--- server/bot/webhook.post.ts
+++ server/bot/webhook.post.ts
@@ -1,23 +1,23 @@
-// import {Telegraf} from 'telegraf'
-// import {getBot} from "#server/bot/index";
-//
-//
-// export default defineEventHandler(async (event) => {
-//
-//     try {
-//
-//         const bot = getBot()
-//
-//         const body = await readBody(event)
-//
-//         await bot.handleUpdate(body)
-//
-//         return {ok: true}
-//
-//     } catch (e) {
-//
-//         console.error('Webhook error:', e)
-//
-//         return {ok: false}
-//     }
-// })
+import {Telegraf} from 'telegraf'
+import {getBot} from "#server/plugins/bot";
+
+
+export default defineEventHandler(async (event) => {
+
+    try {
+
+        const bot = getBot()
+
+        const body = await readBody(event)
+
+        await bot.handleUpdate(body)
+
+        return {ok: true}
+
+    } catch (e) {
+
+        console.error('Webhook error:', e)
+
+        return {ok: false}
+    }
+})
--- server/plugins/bot.ts
+++ server/plugins/bot.ts
@@ -1,25 +1,51 @@
 import { Telegraf } from 'telegraf'
-import { start } from "#server/bot/commands/start"
-import { processTelegramAuth } from "#server/bot/services/auth/processTelegramAuth"
-import { processMediaSearch } from "#server/bot/services/addMedia/processMediaSearch"
-import { selectMedia } from "#server/bot/actions/addMedia/selectMedia"
-import { saveMedia } from "#server/bot/actions/addMedia/saveMedia"
-import {addMovie} from "#server/bot/actions/addMedia";
+import {start} from "#server/bot/commands/start";
+import {processTelegramAuth} from "#server/bot/services/auth/processTelegramAuth";
+import {processMediaSearch} from "#server/bot/services/addMedia/processMediaSearch";
 import {addMediaState} from "#server/bot/consts/addMedia/addMediaState";
-import {getBot} from "#server/bot";
+import {addMovie} from "#server/bot/actions/addMedia";
+import {selectMedia} from "#server/bot/actions/addMedia/selectMedia";
+import {saveMedia} from "#server/bot/actions/addMedia/saveMedia";
 
-const globalForBot = globalThis as any
+let bot: Telegraf | null = null
 
-export default defineNitroPlugin(async () => {
+export const getBot = () => {
 
-    if (globalForBot.telegramBot) return
+    if (bot) return bot
 
     const config = useRuntimeConfig()
 
-    const bot = getBot()
+    bot = new Telegraf(config.telegramKey)
+
+    const authRequests = new Map()
+
+    bot.start(async (ctx) => {
+        await start(ctx, authRequests)
+    })
+
+    bot.action('check_sub', async (ctx) =>
+        await processTelegramAuth(ctx, authRequests)
+    )
+
+    bot.action('add_media', async (ctx) =>
+        await processMediaSearch(ctx, bot, authRequests)
+    )
+
+    bot.on('text', async (ctx) => {
+
+        const state = addMediaState.get(ctx.from.id)
+
+        if (!state?.waitingMovie) {
+            return
+        }
+
+        await addMovie(ctx)
+    })
+
+    bot.action(/^media_(\d+)_(movie|tv)$/, selectMedia)
+
+    bot.action(/^save_(\d+)_(\d+)_(movie|tv)$/, saveMedia)
 
 
-    globalForBot.telegramBot = bot
-
-    console.log('Telegram bot initialized')
-})
+    return bot
+}
