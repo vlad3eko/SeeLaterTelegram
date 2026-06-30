1dc4cc57b7bdcc4dfffa51bfe08705ec935038a8
vlad3eko <vlad3eko@gmail.com>
2026-06-30 20:21:40 +0300

    edit server bot "edit bot"

--- server/bot/handlers/auth/success/sendSubscriptionSuccessMessage.ts
+++ server/bot/handlers/auth/success/sendSubscriptionSuccessMessage.ts
@@ -1,6 +1,10 @@
 import {processMediaSearch} from "#server/bot/services/addMedia/processMediaSearch";
 
 export const sendSubscriptionSuccessMessage = async (ctx: any, authRequests: Map<string, number>) => {
+
+    await ctx.answerCbQuery()
+    await ctx.deleteMessage()
+
     await ctx.reply(
         '✅ Подписка подтверждена \n Спасибо за поддержку проекта!',
     )
