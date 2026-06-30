4b18f77ce87fd11b06482f3a42e505ced4375985
vlad3eko <vlad3eko@gmail.com>
2026-06-30 23:10:12 +0300

    edit server bot "edit bot"

--- server/bot/handlers/auth/fail/failedChannelSubscriber.ts
+++ server/bot/handlers/auth/fail/failedChannelSubscriber.ts
@@ -2,6 +2,10 @@ import {Markup} from "telegraf";
 import {processTelegramAuth} from "#server/bot/services/auth/processTelegramAuth";
 
 export const failedChannelSubscriber = async (ctx: any, authRequests: Map<string, number>) => {
+
+    await ctx.answerCbQuery()
+    await ctx.deleteMessage()
+
     await ctx.reply(
         '❌ Подпишитесь на канал',
         Markup.inlineKeyboard([
@@ -15,4 +19,5 @@ export const failedChannelSubscriber = async (ctx: any, authRequests: Map<string
             )
         ])
     )
+
 }
