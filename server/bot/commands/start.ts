a510a8162ffedefcdedf3bb6d38c0d502e788ef5
vlad3eko <vlad3eko@gmail.com>
2026-07-02 00:28:13 +0300

    edit server bot "edit bot start"

--- server/bot/commands/start.ts
+++ server/bot/commands/start.ts
@@ -10,4 +10,6 @@ export const start = async (ctx: any, authRequests: Map<string, number>) => {
         await processTelegramAuth(ctx, authRequests, true)
     }
     await processTelegramAuth(ctx, authRequests, false)
+
+        await failedLoginToken(ctx)
 }
