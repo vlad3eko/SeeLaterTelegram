4c252263f0dd864f810fd5df65a6351065122eeb
vlad3eko <vlad3eko@gmail.com>
2026-07-02 00:24:42 +0300

    edit server bot "edit bot start"

--- server/bot/handlers/auth/check/checkChannelSubscriber.ts
+++ server/bot/handlers/auth/check/checkChannelSubscriber.ts
@@ -4,13 +4,6 @@ export const checkChannelSubscriber = async (ctx: any) => {
         ctx.from.id
     )
 
-    const member = [
-        'member',
-        'administrator',
-        'creator'
-    ].includes(channelMember.status)
-    console.log('member', member)
-
     return [
         'member',
         'administrator',
--- server/bot/services/auth/processTelegramAuth.ts
+++ server/bot/services/auth/processTelegramAuth.ts
@@ -21,9 +21,6 @@ export const processTelegramAuth = async (ctx: any, authRequests: Map<string, nu
         await confirmUserRequest(ctx, authRequests)
         if (sendSuccessMessage) {
             await sendSubscriptionSuccessMessage(ctx, authRequests)
-        } else {
-            await failedLoginToken(ctx)
-            await ctx.deleteMessage()
         }
 
         return true
