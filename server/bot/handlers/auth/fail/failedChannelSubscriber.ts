2a94b66af7869f2a2dc24fa9ed2678cebd1e546b
vlad3eko <vlad3eko@gmail.com>
2026-07-01 23:15:24 +0300

    edit server bot "edit bot start"

--- server/bot/handlers/auth/fail/failedChannelSubscriber.ts
+++ server/bot/handlers/auth/fail/failedChannelSubscriber.ts
@@ -20,6 +20,6 @@ export const failedChannelSubscriber = async (ctx: any, authRequests: Map<string
         ])
     )
 
-    if (count >= 1) await ctx.deleteMessage()
+    if (count > 1) await ctx.deleteMessage()
 
 }
