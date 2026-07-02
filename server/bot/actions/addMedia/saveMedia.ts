30ef765df52e34a68f42b6b6ee9e1bfce52e25c4
vlad3eko <vlad3eko@gmail.com>
2026-07-02 21:37:23 +0300

    edit server bot "edit bot start/reg"

--- server/bot/actions/addMedia/saveMedia.ts
+++ server/bot/actions/addMedia/saveMedia.ts
@@ -63,8 +63,8 @@ export const saveMedia = async (ctx: any) => {
     const currentId = ctx.message.message_id;
 
     await ctx.telegram.deleteMessages(ctx.chat.id, [
-        currentId - 1,
-        currentId - 2
+        currentId - 2,
+        currentId - 3
     ])
 
     await ctx.reply(
