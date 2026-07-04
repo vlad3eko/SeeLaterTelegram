1e7584048eaa87c25122161fdd2a179ed0d85f3d
vlad3eko <vlad3eko@gmail.com>
2026-07-04 21:33:30 +0300

    edit server bot "bot timer"

--- server/bot/handlers/index.ts
+++ server/bot/handlers/index.ts
@@ -1,6 +1,16 @@
 import {Telegraf} from "telegraf";
 import {nameMediaSearch} from "#server/bot/actions/media/nameMediaSearch";
+import {addSessionMessage} from "#server/bot/services/session/addSessionMessage";
 
 export function registerHandlers(bot: Telegraf) {
-    bot.on('text', nameMediaSearch)
+    bot.on('text', async (ctx) => {
+
+        await addSessionMessage(
+            ctx.from.id,
+            ctx.message.message_id
+        );
+
+        await nameMediaSearch(ctx)
+    })
+
 }
