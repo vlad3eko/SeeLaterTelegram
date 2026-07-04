44bc6c21c579c9055a910fbc6983c7853f5092c7
vlad3eko <vlad3eko@gmail.com>
2026-07-04 17:52:38 +0300

    edit server bot "bot session"

--- server/bot/services/session/addSessionMessage.ts
+++ server/bot/services/session/addSessionMessage.ts
@@ -1,4 +1,7 @@
 export const addSessionMessage = async (telegramId: number, messageId: number) => {
+
+    if (telegramId && messageId) return
+
     await $fetch('/api/bot/session/editAddSessionMessage', {
         method: 'POST',
         body: {
