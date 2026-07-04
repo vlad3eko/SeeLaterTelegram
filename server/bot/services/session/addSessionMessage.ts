cdeafced6aeeadb65283edf1d4a99a1d4134cd98
vlad3eko <vlad3eko@gmail.com>
2026-07-04 18:14:14 +0300

    edit server bot "bot session"

--- server/bot/services/session/addSessionMessage.ts
+++ server/bot/services/session/addSessionMessage.ts
@@ -1,15 +1,10 @@
 export const addSessionMessage = async (telegramId: number, messageId: number, ctx?: any) => {
 
-    console.log('ctx.callback_query.message.message_id', ctx.callbackQuery.message.message_id)
-    console.log('ctx.message.message_id', ctx.message.message_id)
+    console.log('ctx.callback_query.message.message_id', ctx.callbackQuery)
     if ((!telegramId && !messageId)) {
         console.log('возврат', telegramId + ':', messageId)
-    console.log('возврат ctx')
+        return
     }
-    console.log('прошёл ctx')
-    console.log('прошла', telegramId + ':', messageId)
-    console.log('telegramId', telegramId)
-    console.log('messageId', messageId)
 
     await $fetch('/api/bot/session/editAddSessionMessage', {
         method: 'POST',
