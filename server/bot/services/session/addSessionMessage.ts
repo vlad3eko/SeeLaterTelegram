b663106b4154f28ff64baf2cad7b47abcb349a0d
vlad3eko <vlad3eko@gmail.com>
2026-07-04 18:27:33 +0300

    edit server bot "bot session"

--- server/bot/services/session/addSessionMessage.ts
+++ server/bot/services/session/addSessionMessage.ts
@@ -1,6 +1,6 @@
 export const addSessionMessage = async (telegramId: number, messageId: number, ctx?: any) => {
 
-    console.log('ctx', ctx.callback_query.message)
+    console.log('ctx', ctx.message.message_id)
     console.log('telegramId', telegramId)
     console.log('messageId', messageId)
 
