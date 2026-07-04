893373c5bd69d1e68d532359740a64a3a6e3acfe
vlad3eko <vlad3eko@gmail.com>
2026-07-04 18:49:15 +0300

    edit server bot "bot session"

--- server/bot/actions/media/saveMedia.ts
+++ server/bot/actions/media/saveMedia.ts
@@ -94,5 +94,5 @@ export const saveMedia = async (ctx: any) => {
             }
         }
     )
-    await addSessionMessage(ctx.from.id, answer)
+    await addSessionMessage(ctx.from.id, answer.message_id)
 }
