a1cb47bb2db3c44170d139a9462dbcc9ade04313
vlad3eko <vlad3eko@gmail.com>
2026-07-04 13:47:28 +0300

    edit server bot "bot"

--- app/services/movies/supabase.ts
+++ app/services/movies/supabase.ts
@@ -1,1 +1,10 @@
+import {createClient} from "@supabase/supabase-js";
+
 export const supabase = useSupabaseClient()
+
+const config = useRuntimeConfig()
+
+export const supabaseBot = createClient(
+    config.supabase.secretKey,
+    config.supabase.serviceKey
+)
--- server/bot/handlers/channel/isSubscriber.ts
+++ server/bot/handlers/channel/isSubscriber.ts
@@ -1,15 +1,29 @@
 import {checkChannelSubscriber} from "#server/bot/handlers/auth/check/checkChannelSubscriber";
 import {failedChannelSubscriber} from "#server/bot/handlers/auth/fail/failedChannelSubscriber";
+import {supabaseBot} from "~/services/movies/supabase";
 
 export const isSubscriber = async (ctx: any) => {
     const isChannelSubscriber = await checkChannelSubscriber(ctx)
 
-
-
     if (!isChannelSubscriber) {
         await failedChannelSubscriber(ctx)
         await ctx.deleteMessage()
+            // ... здесь код который меняет значение subscriber = false
+        await supabaseBot
+            .from('users')
+            .update({
+                subscriber: false
+            })
+            .eq('telegram_id', ctx.from.id)
         return false
+    } else {
+        // ...  ... здесь код который меняет значение subscriber = true
+        await supabaseBot
+            .from('users')
+            .update({
+                subscriber: true
+            })
+            .eq('telegram_id', ctx.from.id)
     }
 
     return true
