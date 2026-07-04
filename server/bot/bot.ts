8d4ecebf3f60a1597e1b61251ffdb95aeabf57ce
vlad3eko <vlad3eko@gmail.com>
2026-07-04 22:51:06 +0300

    edit server bot "bot timer"

--- server/bot/bot.ts
+++ server/bot/bot.ts
@@ -1,7 +1,7 @@
-import {Telegraf} from 'telegraf'
 import {registerActions} from "#server/bot/actions";
 import {registerCommands} from "#server/bot/commands";
 import {registerHandlers} from "#server/bot/handlers";
+import {Telegraf} from "telegraf";
 
 export const bot = new Telegraf(process.env.TELEGRAM_TOKEN!)
 
