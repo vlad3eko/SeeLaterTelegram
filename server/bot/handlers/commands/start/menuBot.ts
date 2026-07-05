c5b31607e5e4b08bd8975739b96ef365b07aa484
vlad3eko <vlad3eko@gmail.com>
2026-07-05 17:07:49 +0300

    edit server bot "bot fix"

--- server/bot/handlers/commands/start/menuBot.ts
+++ server/bot/handlers/commands/start/menuBot.ts
@@ -8,10 +8,10 @@ export const menuBot = async (ctx: any) => {
 
     const message = await ctx.reply(`Добро пожаловать ${ctx.from.first_name || ctx.from.username}`,
         Markup.inlineKeyboard([
-            Markup.button.url(
-                'Перейти на сайт',
-                'https://see-later-telegram.vercel.app/bookmarks'
-            ),
+            // Markup.button.url(
+            //     'Перейти на сайт',
+            //     'https://see-later-telegram.vercel.app/bookmarks'
+            // ),
             Markup.button.callback(
                 'Поиск',
                 'search_media'
