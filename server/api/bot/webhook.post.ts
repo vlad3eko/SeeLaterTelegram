0fcd9227152bf8cdcb685feb821bcba7c5146470
vlad3eko <vlad3eko@gmail.com>
2026-07-05 16:05:04 +0300

    edit server bot "bot fix"

--- server/api/bot/webhook.post.ts
+++ server/api/bot/webhook.post.ts
@@ -1,10 +1,11 @@
 import {bot} from "#server/bot/bot";
 
 export default defineEventHandler(async (event) => {
+    console.log('🔥 WEBHOOK HIT')
     const body = await readBody(event)
-
     try {
-        console.log('bot', bot)
+        await bot.handleUpdate(body)
+        console.log('BODY:', body)
         return { ok: true }
     } catch (err: any) {
         console.error('Telegram webhook error:', err)
