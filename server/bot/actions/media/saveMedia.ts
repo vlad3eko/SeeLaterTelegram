c51f4a372fc8c0e9a7b0d8484084be5769100bff
vlad3eko <vlad3eko@gmail.com>
2026-07-05 17:21:44 +0300

    edit server bot "bot fix"

--- server/bot/actions/media/saveMedia.ts
+++ server/bot/actions/media/saveMedia.ts
@@ -52,7 +52,7 @@ export const saveMedia = async (ctx: any) => {
 
         if (error?.message.includes('duplicate key value')) {
 
-            await deleteMessages(ctx, [0, -2])
+            await deleteMessages(ctx, [1, -1])
 
             const message = await ctx.reply(
                 `❌ Ой: вы уже сохраняли - ${media.title || media.name}`,
