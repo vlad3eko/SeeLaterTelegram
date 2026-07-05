01e8b53869c770f347af5a017d5daba006b2b08c
vlad3eko <vlad3eko@gmail.com>
2026-07-05 17:11:03 +0300

    edit server bot "bot fix"

--- server/bot/actions/media/saveMedia.ts
+++ server/bot/actions/media/saveMedia.ts
@@ -108,7 +108,7 @@ export const saveMedia = async (ctx: any) => {
         }
     )
     if (successSave) {
-        await deleteMessages(ctx, [1])
+        await deleteMessages(ctx, [0])
     }
     await saveMessageSession(ctx.from.id, successSave.message_id)
 }
