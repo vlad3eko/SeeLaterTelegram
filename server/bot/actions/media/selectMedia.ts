2b2c15247762b5df0bb2f4789e01503c84213c36
vlad3eko <vlad3eko@gmail.com>
2026-07-06 23:29:44 +0300

    edit server bot "bot fix"

--- server/bot/actions/media/selectMedia.ts
+++ server/bot/actions/media/selectMedia.ts
@@ -1,20 +1,12 @@
 import {sendMediaCard} from "#server/bot/consts/media/sendMediaCard";
-import {addMediaState} from "#server/bot/consts/media/addMediaState";
-import {deleteMessages} from "#server/bot/actions/delete/deleteMessages";
 
 export const selectMedia = async (ctx: any) => {
 
     await ctx.answerCbQuery()
-    const state = addMediaState.get(ctx.from.id)
-
 
     const mediaId = Number(ctx.match[1])
     const mediaType = String(ctx.match[2])
 
  await sendMediaCard(ctx, mediaId, mediaType)
 
-    if (!state?.waitingMovie) {
-        await deleteMessages(ctx, [-1])
-
-    }
 }
