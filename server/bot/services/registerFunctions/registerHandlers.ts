4f8270e5d2d022e31f539d12fea4cea3081562a6
vlad3eko <vlad3eko@gmail.com>
2026-07-06 23:36:23 +0300

    edit server bot "bot fix"

--- server/bot/services/registerFunctions/registerHandlers.ts
+++ server/bot/services/registerFunctions/registerHandlers.ts
@@ -13,28 +13,30 @@ export function registerHandlers(bot: Telegraf) {
         const text = ctx.message.text
         const state = addMediaState.get(ctx.from.id)
 
+        if (text.startsWith('/')) return
+
         if (text.startsWith("media_")) {
             await openInlineMovie(ctx)
             return
         }
 
-        // if (!state?.waitingMovie) {
-        //
-        //     const message = await ctx.reply(
-        //         `🔍 Искать фильм «${text}» через быстрый поиск?`,
-        //         Markup.inlineKeyboard([
-        //             [
-        //                 Markup.button.switchToCurrentChat(
-        //                     `🔍 Искать «${text}»`,
-        //                     text
-        //                 )
-        //             ]
-        //         ])
-        //     )
-        //     await deleteMessages(ctx, [message.message_id - 1])
-        //     await addMessageSession(ctx.from.id, message.message_id)
-        //     return
-        // }
+        if (!state?.waitingMovie) {
+
+            const message = await ctx.reply(
+                `🔍 Искать фильм «${text}» через быстрый поиск?`,
+                Markup.inlineKeyboard([
+                    [
+                        Markup.button.switchToCurrentChat(
+                            `🔍 Искать «${text}»`,
+                            text
+                        )
+                    ]
+                ])
+            )
+            await deleteMessages(ctx, [message.message_id - 1])
+            await addMessageSession(ctx.from.id, message.message_id)
+            return
+        }
 
         await addMessageSession(ctx.from.id, ctx.message.message_id)
         await nameMediaSearch(ctx)
