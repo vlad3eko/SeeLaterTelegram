2bb73e5fe5e2cf0899c7b025b43fcd16d95886f4
vlad3eko <vlad3eko@gmail.com>
2026-07-08 00:58:16 +0300

    edit server bot

--- server/bot/commands/commandClear.ts
+++ server/bot/commands/commandClear.ts
@@ -1,7 +1,5 @@
 import {deleteOldMessagesSession} from "#server/bot/services/session/deleteOldMessagesSession";
 
 export async function commandClear(ctx: any) {
-    console.log('body.id', ctx.from.id)
-
     await deleteOldMessagesSession(ctx.from.id)
 }
--- server/bot/services/session/deleteOldMessagesSession.ts
+++ server/bot/services/session/deleteOldMessagesSession.ts
@@ -1,6 +1,8 @@
 export const deleteOldMessagesSession = async (telegramId: any) => {
     await $fetch('/api/bot/session/deleteOldMessagesSession', {
         method: 'POST',
-        telegram_id: telegramId
+        body: {
+            telegram_id: telegramId
+        }
     })
 }
