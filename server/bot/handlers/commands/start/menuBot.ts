c704d2468ef2a36e7f67f6ff472e291a5bbd18bc
vlad3eko <vlad3eko@gmail.com>
2026-07-07 20:29:47 +0300

    edit server bot "bot fix"

--- server/bot/handlers/commands/start/menuBot.ts
+++ server/bot/handlers/commands/start/menuBot.ts
@@ -4,7 +4,6 @@ import {SessionMessageType} from "#server/bot/consts/types/SessionMessageTypes";
 
 export const menuBot = async (ctx: any) => {
 
-    await ctx.answerCbQuery()
     await ctx.deleteMessage()
 
     const message = await ctx.reply(`🍿 Привет ${ctx.from.first_name || ctx.from.username}\n
