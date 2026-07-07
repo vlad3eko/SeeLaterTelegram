f7c02e043643a1a08b5b6c334127b0cb1e175e29
vlad3eko <vlad3eko@gmail.com>
2026-07-07 20:31:11 +0300

    edit server bot "bot fix"

--- server/bot/handlers/commands/start/menuBot.ts
+++ server/bot/handlers/commands/start/menuBot.ts
@@ -6,29 +6,22 @@ export const menuBot = async (ctx: any) => {
 
     await ctx.deleteMessage()
 
-    const message = await ctx.reply(`🍿 Привет ${ctx.from.first_name || ctx.from.username}\n
+    const message = await ctx.reply(
+        `🍿 Привет ${ctx.from.first_name || ctx.from.username}
+
 🔍 Для поиска используй кнопки ниже или отправь в сообщении название кино`,
         {
-            reply_markup:
-                Markup.inlineKeyboard([
-                    // Markup.button.url(
-                    //     'Перейти на сайт',
-                    //     'https://see-later-telegram.vercel.app/bookmarks'
-                    // ),
-                    [
-                        Markup.button.switchToCurrentChat(
-                            `🔍 Поиск`,
-                            ''
-                        )
-                    ]
-                    // Markup.button.callback(
-                    //     'Сохранённые',
-                    //     'search_media'
-                    // )
-                ]),
-
+            reply_markup: Markup.inlineKeyboard([
+                [
+                    Markup.button.switchToCurrentChat(
+                        '🔍 Поиск',
+                        ''
+                    )
+                ]
+            ]).reply_markup
         }
     )
+
     await addMessageSession(
         ctx.from.id,
         message.message_id,
