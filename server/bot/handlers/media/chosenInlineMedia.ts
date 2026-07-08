d406cfaec1f6b4c40b0ed5165cdc8bf148f6dba0
vlad3eko <vlad3eko@gmail.com>
2026-07-09 01:55:19 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -1,14 +1,27 @@
 import { SessionMessageType } from "#server/bot/consts/types/SessionMessageTypes";
-import {addInlineMessageSession} from "#server/bot/services/session/addInlineMessageSession";
+import { addInlineMessageSession } from "#server/bot/services/session/addInlineMessageSession";
 
 export const chosenInlineMedia = async (ctx: any) => {
 
+    console.log("========== CHOSEN INLINE ==========")
+    console.dir(ctx.update, { depth: null })
+
     const result = ctx.chosenInlineResult
-    if (!result.inline_message_id) return
+
+    console.log("RESULT:", result)
+
+    if (!result?.inline_message_id) {
+        console.log("NO INLINE MESSAGE ID")
+        return
+    }
+
+    console.log("INLINE ID:", result.inline_message_id)
 
     await addInlineMessageSession(
         ctx.from.id,
         result.inline_message_id,
         SessionMessageType.InlineMediaCard
     )
+
+    console.log("SESSION SAVED")
 }
