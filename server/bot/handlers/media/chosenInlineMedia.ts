d2ca10e3dac27290d641fd53f6f527f12fe15e4d
vlad3eko <vlad3eko@gmail.com>
2026-07-10 20:05:53 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -1,12 +1,118 @@
-import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBot";
 import {createMediaCaption} from "#server/bot/consts/media/createMediaCaption";
+import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBot";
+
+export const chosenInlineMedia = async (ctx:any)=>{
+
+    try {
+
+        const result =
+            ctx.chosenInlineResult
 
 
-export const chosenInlineMedia = async(ctx:any)=>{
+        console.log(
+            'chosen result',
+            result
+        )
 
-    console.log('res', ctx.chosenInlineResult)
-    console.log('res', ctx.telegram.chosenInlineResult)
-    console.log('res await', await ctx.chosenInlineResult)
 
-    console.log('sec', ctx.chosen_inline_result)
+        const inlineMessageId =
+            result.inline_message_id
+
+
+        const resultId =
+            result.result_id
+
+
+        if(!inlineMessageId || !resultId)
+            return
+
+
+
+        const [
+            mediaType,
+            mediaId
+        ] =
+            resultId.split('_')
+
+
+
+        const media = await $fetch(
+            '/api/bot/media/getMediaBot',
+            {
+                query:{
+                    media: mediaType,
+                    id: mediaId
+                }
+            }
+        )
+
+
+
+        await new Promise(resolve =>
+            setTimeout(resolve,300)
+        )
+
+
+
+        await ctx.telegram.editMessageMedia(
+
+            undefined,
+
+            undefined,
+
+            inlineMessageId,
+
+
+            {
+                type:'photo',
+
+                media:
+                    `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+
+                caption:
+                    createMediaCaption(
+                        media,
+                        mediaType
+                    ),
+
+                parse_mode:'HTML'
+            },
+
+
+            {
+                reply_markup:
+                    keyboardSendMediaCardInline(
+                        mediaId,
+                        mediaType
+                    )
+            }
+        )
+
+
+
+    } catch(e){
+
+        console.log(
+            'chosenInlineMedia error',
+            e
+        )
+
+    }
+
 }
+
+/*
+* await ctx.telegram.editMessageMedia(
+            undefined,
+            undefined,
+            inlineMessageId,
+            {
+                type: 'photo',
+                media: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+                caption: createMediaCaption(media, media.media_type),
+                parse_mode: 'HTML',
+            },
+            {
+                reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
+            }
+        )*/
