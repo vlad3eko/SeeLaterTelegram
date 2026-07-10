1933602d2e47331be80361baaaae6f1e42e74a26
vlad3eko <vlad3eko@gmail.com>
2026-07-10 17:10:30 +0300

    edit server bot refactor

--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -5,8 +5,11 @@ export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
     try {
         const results = medias.results.map((media: any) => ({
-            type: 'photo',
+            type: 'article',
             id: `${media.media_type}_${media.id}`,
+            title: media.title || media.name,
+
+            description: "Тестовое описание",
             photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
             thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
 
