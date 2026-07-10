c9fe66ce441ec5caebb25b95fc23713dec3d13ad
vlad3eko <vlad3eko@gmail.com>
2026-07-10 20:09:18 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -3,101 +3,10 @@ import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBo
 
 export const chosenInlineMedia = async (ctx:any)=>{
 
-    try {
+    const result = ctx.chosenInlineResult
+    console.log('chosen results', result)
 
-        const result =
-            ctx.chosenInlineResult
-
-
-        console.log(
-            'chosen result',
-            result
-        )
-
-
-        const inlineMessageId =
-            result.inline_message_id
-
-
-        const resultId =
-            result.result_id
-
-
-        if(!inlineMessageId || !resultId)
-            return
-
-
-
-        const [
-            mediaType,
-            mediaId
-        ] =
-            resultId.split('_')
-
-
-
-        const media = await $fetch(
-            '/api/bot/media/getMediaBot',
-            {
-                query:{
-                    media: mediaType,
-                    id: mediaId
-                }
-            }
-        )
-
-
-
-        await new Promise(resolve =>
-            setTimeout(resolve,300)
-        )
-
-
-
-        await ctx.telegram.editMessageMedia(
-
-            undefined,
-
-            undefined,
-
-            inlineMessageId,
-
-
-            {
-                type:'photo',
-
-                media:
-                    `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-
-                caption:
-                    createMediaCaption(
-                        media,
-                        mediaType
-                    ),
-
-                parse_mode:'HTML'
-            },
-
-
-            {
-                reply_markup:
-                    keyboardSendMediaCardInline(
-                        mediaId,
-                        mediaType
-                    )
-            }
-        )
-
-
-
-    } catch(e){
-
-        console.log(
-            'chosenInlineMedia error',
-            e
-        )
-
-    }
+    if (!result.inline_message_id) return
 
 }
 
