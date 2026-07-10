309a464b891db53b2ec94e3bbdff4fa8f529ec6c
vlad3eko <vlad3eko@gmail.com>
2026-07-10 17:06:56 +0300

    edit server bot refactor

--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -6,25 +6,16 @@ export const processSearchMediaInline = async (ctx: any, medias: any) => {
     try {
         const results = medias.results.map((media: any) => ({
             type: 'article',
-
             id: `${media.media_type}_${media.id}`,
-
-            photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-            thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-
             title: media.title || media.name,
 
-
-            description: [
-                media.media_type === 'movie' ? 'фильм' : 'сериал',
-                media.vote_average ? `★ ${media.vote_average.toFixed(1)}` : '',
-                media.release_date?.slice(0, 4) || media.first_air_date?.slice(0, 4)
-            ].filter(Boolean).join(' • '),
+            description: "Тестовое описание",
+            photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+            thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
 
             caption: createMediaCaption(media, media.media_type),
 
             parse_mode: 'HTML',
-
             reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
         }))
 
