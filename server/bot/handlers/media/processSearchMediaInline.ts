fa2742aea8ab4826e31b7120f5c99e89438572ed
vlad3eko <vlad3eko@gmail.com>
2026-07-10 17:22:09 +0300

    edit server bot refactor

--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -4,20 +4,59 @@ import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBo
 export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
     try {
-        const results = medias.results.map((media: any) => ({
-            type: 'photo',
-            id: `${media.media_type}_${media.id}`,
-            title: media.title || media.name,
+        const results = medias.results.map((media: any) => {
 
-            description: "Тестовое описание",
-            photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-            thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+            const year =
+                (media.release_date || media.first_air_date || '')
+                    .slice(0, 4)
 
-            caption: createMediaCaption(media, media.media_type),
+            const title =
+                media.title || media.name
 
-            parse_mode: 'HTML',
-            reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
-        }))
+            const type =
+                media.media_type === 'movie'
+                    ? 'фильм'
+                    : 'сериал'
+
+            const genres =
+                media.genres
+                    ?.map((g: any) => g.name)
+                    .slice(0, 3)
+                    .join(', ')
+
+            return {
+
+                type: 'photo',
+
+                id: `${media.media_type}_${media.id}`,
+
+                title,
+
+                description:
+                    `${type} | imdb:${media.vote_average?.toFixed(1)} | ${year}
+${genres}`,
+
+                photo_url:
+                    `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+
+                thumbnail_url:
+                    `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+
+                caption: createMediaCaption(
+                    media,
+                    media.media_type
+                ),
+
+                parse_mode: 'HTML',
+
+                reply_markup:
+                    keyboardSendMediaCardInline(
+                        media.id,
+                        media.media_type
+                    )
+            }
+
+        })
 
         console.log('result', results[0])
         await ctx.answerInlineQuery(results)
