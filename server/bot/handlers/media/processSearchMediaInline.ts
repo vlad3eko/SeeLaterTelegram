a74b5766889daa0411e12aa0e48874be245c76ba
vlad3eko <vlad3eko@gmail.com>
2026-07-10 17:48:36 +0300

    edit server bot refactor

--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -6,7 +6,7 @@ export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
     try {
         const results = medias.results.map((media: any) => ({
-            type: 'photo',
+            type: 'article',
 
             id: String(media.id),
 
