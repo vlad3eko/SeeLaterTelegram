49977473c986b145f1d2ecac8a4375d6d834ed6a
vlad3eko <vlad3eko@gmail.com>
2026-07-10 21:03:51 +0300

    edit server bot refactor

---
+++ app/utils/convert/genresConvert.ts
@@ -0,0 +1,60 @@
+import type { TmdbGenre } from "~/types/tmdb.types";
+
+
+export const genresConvert = (
+    genres: TmdbGenre[] = []
+) => {
+
+    if (!genres.length) {
+        return ''
+    }
+
+
+    const formatGenre = (name: string) => {
+
+        return name
+            .split(' ')
+            .filter(Boolean)
+            .map((word, index) => {
+
+                if (index === 0) {
+                    return word
+                }
+
+                return (
+                    word.charAt(0).toUpperCase() +
+                    word.slice(1)
+                )
+
+            })
+            .join('')
+            .replace(
+                /[^а-яА-Яa-zA-Z0-9]/g,
+                ''
+            )
+    }
+
+
+    return genres
+        .map((genre) => {
+
+            const words =
+                genre.name.split(' ')
+
+
+            // "НФ и Фэнтези" -> "#НФ #Фэнтези"
+            if (words.includes('и')) {
+
+                return words
+                    .filter(word => word !== 'и')
+                    .map(word => `#${formatGenre(word)}`)
+                    .join(' ')
+            }
+
+
+            return `#${formatGenre(genre.name)}`
+
+        })
+        .filter(Boolean)
+        .join(' ')
+}
--- server/bot/consts/media/createMediaCaption.ts
+++ server/bot/consts/media/createMediaCaption.ts
@@ -1,12 +1,11 @@
 import {FormatDate} from "~/utils/formatMoviesData";
 import {mediaReleaseConvert, mediaTypeConvert} from "~/utils/convert/mediaConvert";
 import type {TmdbGenre} from "~/types/tmdb.types";
+import {genresConvert} from "~/utils/convert/genresConvert";
 
 export const createMediaCaption = (media: any, mediaType: string) => {
 
-    const genresContent = (media.genres || [])
-        .map((i: TmdbGenre) => i.name)
-        .join(' • #') || 'нет данных'
+    const genresContent = genresConvert(media.genres)
 
     const mediaTitle = `<code>${media.title || media.name} (${FormatDate(media.release_date || media.first_air_date) || '❌официальной даты пока нет'})</code> `
     const mediaOverview = media.overview?.length > 350
@@ -16,7 +15,7 @@ export const createMediaCaption = (media: any, mediaType: string) => {
 
     return `${mediaTitle}
                 <blockquote expandable>${mediaOverview}</blockquote>\n
-<b>Жанр: </b><i>#${genresContent}</i>
+<b>Жанр: </b><i>#${genresContent || 'нет жанров'}</i>
 <b>Тип: </b><i>#${mediaTypeConvert(mediaType)}</i>
 <b>Дата выхода: </b><i>${mediaReleaseConvert(media)}</i>\n
 <b><i>${channelLink}</i></b>`
