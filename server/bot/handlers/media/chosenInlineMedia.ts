53a4d62ad52c4afad84d01285d16db8a5155ad2b
vlad3eko <vlad3eko@gmail.com>
2026-07-10 19:45:59 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -1,118 +1,70 @@
 import {createMediaCaption} from "#server/bot/consts/media/createMediaCaption";
 import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBot";
 
-export const chosenInlineMedia = async (ctx:any)=>{
+export const chosenInlineMedia = async (ctx: any) => {
 
-    try {
-
-        const result =
-            ctx.chosenInlineResult
+    const result =
+        ctx.chosenInlineResult
 
 
-        console.log(
-            'chosen result',
-            result
-        )
+    console.log(
+        'chosen result',
+        result
+    )
 
 
-        const inlineMessageId =
-            result.inline_message_id
+    const inlineMessageId =
+        result.inline_message_id
 
 
-        const resultId =
-            result.result_id
+    const resultId =
+        result.result_id
 
 
-        if(!inlineMessageId || !resultId)
-            return
+    if (!inlineMessageId || !resultId)
+        return
 
 
-
-        const [
-            mediaType,
-            mediaId
-        ] =
-            resultId.split('_')
-
+    const [
+        mediaType,
+        mediaId
+    ] =
+        resultId.split('_')
 
 
-        const media = await $fetch(
-            '/api/bot/media/getMediaBot',
-            {
-                query:{
-                    media: mediaType,
-                    id: mediaId
-                }
+    const media = await $fetch(
+        '/api/bot/media/getMediaBot',
+        {
+            query: {
+                media: mediaType,
+                id: mediaId
             }
-        )
+        }
+    )
 
 
-
-        await new Promise(resolve =>
-            setTimeout(resolve,300)
-        )
-
+    await new Promise(resolve =>
+        setTimeout(resolve, 300)
+    )
 
 
-        await ctx.telegram.editMessageMedia(
+    await ctx.telegram.editMessageMedia(
+        undefined,
+        undefined,
+        inlineMessageId,
+        {
+            type: 'photo',
+            media: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+            caption: createMediaCaption(media, mediaType),
+            parse_mode: 'HTML',
+        },
+        {
+            reply_markup: keyboardSendMediaCardInline(mediaId, mediaType)
+        }
+    )
 
-            undefined,
-
-            undefined,
-
-            inlineMessageId,
-
-
-            {
-                type:'photo',
-
-                media:
-                    `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-
-                caption:
-                    createMediaCaption(
-                        media,
-                        mediaType
-                    ),
-
-                parse_mode:'HTML'
-            },
-
-
-            {
-                reply_markup:
-                    keyboardSendMediaCardInline(
-                        mediaId,
-                        mediaType
-                    )
-            }
-        )
-
-
-
-    } catch(e){
-
-        console.log(
-            'chosenInlineMedia error',
-            e
-        )
-
-    }
 
 }
 
 /*
-* await ctx.telegram.editMessageMedia(
-            undefined,
-            undefined,
-            inlineMessageId,
-            {
-                type: 'photo',
-                media: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-                caption: createMediaCaption(media, media.media_type),
-                parse_mode: 'HTML',
-            },
-            {
-                reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
-            }
-        )*/
+* */
--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -4,15 +4,15 @@ export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
     try {
         const results = medias.results.map((media:any)=>({
-            type:'article',
-            id:  `${media.media_type}_${media.id}`,
+            type: 'article',
+            id: `${media.media_type}_${media.id}`,
 
             title:  media.title || media.name,
-            description:  `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`,
-            thumb_url:  `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+            description: `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`,
+            thumb_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
 
-            message_text:  'bot: загрузка карточки...',
-            parse_mode:'HTML'
+            message_text: 'bot: загрузка карточки...',
+            parse_mode: 'HTML'
         }))
 
         await ctx.answerInlineQuery(
