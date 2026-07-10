edd70738438849c5fc087d827904325ac1c80bcf
vlad3eko <vlad3eko@gmail.com>
2026-07-10 20:48:54 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -2,59 +2,32 @@ import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBo
 import {createMediaCaption} from "#server/bot/consts/media/createMediaCaption";
 
 
-export const chosenInlineMedia = async(ctx:any)=>{
+export const chosenInlineMedia = async (ctx: any) => {
 
     try {
 
-        const result =
-            ctx.chosenInlineResult
+        const result = ctx.chosenInlineResult
+        const inlineMessageId = result.inline_message_id
 
+        if (!inlineMessageId) return
 
-        console.log(
-            'chosen result:',
-            result
-        )
+        const [mediaType, mediaId] = result.result_id.split('_')
 
-
-        const inlineMessageId =
-            result.inline_message_id
-
-
-        if(!inlineMessageId){
-            console.log(
-                'Нет inline_message_id'
-            )
-            return
-        }
-
-
-        const [
-            mediaType,
-            mediaId
-        ] =
-            result.result_id.split('_')
-
-
-        const media =
-            await $fetch(
-                '/api/bot/getMediaBot',
-                {
-                    query:{
-                        media:mediaType,
-                        id:mediaId
-                    }
+        const media = await $fetch(
+            '/api/bot/getMediaBot',
+            {
+                query: {
+                    media: mediaType,
+                    id: mediaId
                 }
-            )
+            }
+        )
 
         await new Promise(resolve =>
-            setTimeout(resolve,800)
+            setTimeout(resolve, 300)
         )
 
-
-        await ctx.telegram.editMessageMedia(
-            undefined,
-            undefined,
-            inlineMessageId,
+        await ctx.telegram.editMessageMedia(undefined, undefined, inlineMessageId,
             {
                 type: 'photo',
                 media: `https://image.tmdb.org/t/p/w500${media.poster_path || media.backdrop_path}`,
@@ -66,16 +39,7 @@ export const chosenInlineMedia = async(ctx:any)=>{
             }
         )
 
-
-    } catch(e){
-
-        console.log(
-            'chosenInlineMedia error:',
-            e
-        )
+    } catch (e) {
+        console.log('chosenInlineMedia error:', e)
     }
-
 }
-
-/*
-* */
--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -1,63 +1,23 @@
 import {FormatDate} from "~/utils/formatMoviesData";
 import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBot";
 
-export const processSearchMediaInline = async (
-    ctx: any,
-    medias: any
-) => {
+export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
     try {
-        const results = medias.results.map((media:any)=>({
-            type:'article',
-            id:  `${media.media_type}_${media.id}`,
-            title:  media.title || media.name,
-            description:  `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`,
-            thumb_url:  `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-            message_text:  'Загрузка карточки...',
+        const results = medias.results.map((media: any) => ({
+            type: 'article',
+            id: `${media.media_type}_${media.id}`,
+            title: media.title || media.name,
+            description: `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`,
+            thumb_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
+            message_text: 'bot: Загрузка карточки...',
             reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
         }))
 
-
         await ctx.answerInlineQuery(results)
-    } catch(e){
+
+    } catch (e) {
         console.log('Ошибка inline:', e)
         await ctx.answerInlineQuery([])
     }
 }
-
-/*
-const results = medias.results.map((media: any) => ({
-            type: 'photo',
-
-            id: String(media.id),
-
-            photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-            thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-
-            title: media.title || media.name, // не отображается
-
-            description: `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`, // не отображается
-
-            caption: createMediaCaption(media, media.media_type), // в посте есть картинка
-            parse_mode: 'HTML',
-            reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
-        }))
-*/
-
-// Из этого варианта мне нужно чтобы был красивые поиск с вертикальными блоками картика+описание
-// const results = medias.results.map((media: any) => ({
-//     type: 'article',
-//
-//     id: String(media.id),
-//
-//     photo_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-//     thumbnail_url: `https://image.tmdb.org/t/p/w500${media.poster_path}`,
-//
-//     title: media.title || media.name, // отображается
-//
-//     description: `${media.media_type} | imdb 7.9 | (${FormatDate(media.release_date || media.first_air_date) || '❌ отсутствует'})`, // отображается
-//
-//     message_text: createMediaCaption(media, media.media_type), // в посте нет картинки
-//     parse_mode: 'HTML',
-//     reply_markup: keyboardSendMediaCardInline(media.id, media.media_type)
-// }))
