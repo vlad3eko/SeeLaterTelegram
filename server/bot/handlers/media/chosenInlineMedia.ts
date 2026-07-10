706edd03f8cadc67b425f3a25e3562130e896514
vlad3eko <vlad3eko@gmail.com>
2026-07-10 20:03:04 +0300

    edit server bot refactor

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -5,6 +5,7 @@ import {createMediaCaption} from "#server/bot/consts/media/createMediaCaption";
 export const chosenInlineMedia = async(ctx:any)=>{
 
     console.log('res', ctx.chosenInlineResult)
+    console.log('res', ctx.telegram.chosenInlineResult)
     console.log('res await', await ctx.chosenInlineResult)
 
     console.log('sec', ctx.chosen_inline_result)
