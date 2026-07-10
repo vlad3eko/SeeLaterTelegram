1386d1c898e395c8ac98137f12bd07f6dba0ba30
vlad3eko <vlad3eko@gmail.com>
2026-07-10 19:34:13 +0300

    edit server bot refactor

--- server/bot/services/registerFunctions/registerHandlers.ts
+++ server/bot/services/registerFunctions/registerHandlers.ts
@@ -12,9 +12,9 @@ export function registerHandlers(bot: Telegraf) {
         const textId = ctx.message.message_id
 
         if (text.startsWith('/')) return
-        if (text.startsWith('bot:')) {
-            return chosenInlineMedia
-        }
+        // if (text.startsWith('bot:')) {
+        //     return chosenInlineMedia
+        // }
 
         await addMessageSession(ctx.from.id, SessionMessageType.SearchInline, {messageId: textId})
         const message = await ctx.reply(
