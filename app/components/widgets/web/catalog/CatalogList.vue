962e38c608afa6009978c17efa7f9edf9a2c5543
vlad3eko <vlad3eko@gmail.com>
2026-07-11 16:12:20 +0300

    edit server bot refactor architecture

--- app/components/widgets/web/catalog/CatalogList.vue
+++ app/components/widgets/web/catalog/CatalogList.vue
@@ -1,13 +1,20 @@
 <template>
-  <Loader v-if="props.loading"/>
-
   <Transition name="fade" mode="out-in">
-    <section v-if="!props.loading" class="media-grid">
-      <div v-for="media in props.media" :key="media.id" class="media-card">
-        <CatalogCard :media="media" :show-info="props.showInfo"/>
+    <section class="media-grid">
+
+      <div v-for="media in props.media"
+          :key="media.id"
+          class="media-card">
+
+        <CatalogCard
+            :media="media"
+            :show-info="props.showInfo"/>
       </div>
+
     </section>
   </Transition>
+
+  <Loader v-if="props.loading"/>
 </template>
 
 <script lang="ts" setup>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,26 +5,47 @@
       :loading="pending"
       :show-info="true"
   />
+  <div ref="loadMoreTrigger"></div>
 </template>
 
 <script lang="ts" setup>
 import SearchPanel from "~/components/layout/SearchPanel.vue";
 import CatalogList from "~/components/widgets/web/catalog/CatalogList.vue";
 import {useTmdbSearch} from "~/composables/useTmdbSearch";
-import {sortMediaResults} from "~/utils/media/sortMediaResults";
 
 const {
   movies,
   pending,
   searchInput,
-  searchMovies
+  searchMovies,
+  loadMore
 } = useTmdbSearch()
 
 
-const filterMedia = computed(() => {
-  return sortMediaResults(movies.value)
+const loadMoreTrigger = ref<HTMLElement | null>(null)
+let observer: IntersectionObserver | null = null
+
+onMounted(() => {
+
+  if (!loadMoreTrigger.value) return
+
+  observer = new IntersectionObserver(async ([entry]) => {
+
+        if (entry.isIntersecting && !pending.value && movies.value.length) {
+
+          await loadMore()
+        }
+
+      },
+      {
+        rootMargin: "300px"
+      }
+  )
+
+  observer.observe(loadMoreTrigger.value)
 })
 
+onUnmounted(() =>  observer?.disconnect())
 
 </script>
 
