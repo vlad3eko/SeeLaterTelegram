6a58265a595dff1b6067b73dd676b253bcc751a7
vlad3eko <vlad3eko@gmail.com>
2026-07-12 14:43:51 +0300

    edit server bot refactor favorites

--- server/bot/actions/media/favoritesMedia.ts
+++ server/bot/actions/media/favoritesMedia.ts
@@ -1,5 +1,6 @@
-export const favoritesMedia = (user: number) => {
+export const favoritesMedia = (ctx:any, user: number) => {
 
     console.log('ctx.from.id', ctx.from.id)
     console.log('user', ctx.match[1])
+    console.log('callback', ctx.callback_query.from.id)
 }
