2fb8b320ff7a5299d06d9cc096fc26cede98bdc3
vlad3eko <vlad3eko@gmail.com>
2026-07-12 14:52:47 +0300

    edit server bot refactor favorites

--- server/bot/actions/media/favoritesMedia.ts
+++ server/bot/actions/media/favoritesMedia.ts
@@ -2,11 +2,5 @@ export const favoritesMedia = (ctx:any, user: number) => {
 
     const userId = ctx.match[1]
 
-    const data = await $fetch('/api/:media', {
-            query: {
-                userId
-            }
-        })
-
-    console.log('data', data)
+    console.log('userId', userId)
 }
