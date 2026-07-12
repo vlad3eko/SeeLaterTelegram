de14635a91dcb77a78550a34fb150d432127d68b
vlad3eko <vlad3eko@gmail.com>
2026-07-12 19:13:08 +0300

    edit server bot refactor favorites

--- app/utils/media/sortMediaResults.ts
+++ app/utils/media/sortMediaResults.ts
@@ -7,81 +7,80 @@ export const sortMediaResults = (
     userId: number
 ) => {
 
-    const today = Date.now()
-
-    console.log('before result.results', medias)
-
-    const source = medias
-        .filter(filterMediaQuality)
-        .filter(
-            media =>
-                !onlyLiquid || isLiquidMedia(media, userId)
-        )
-    console.log('after result.results', medias)
-
-    const enriched = source.map(item => {
-
-        const releaseDate = new Date(
-            item.release_date ||
-            item.first_air_date ||
-            0
-        ).getTime()
-
-        return {
-
-            ...item,
-
-            _vote:
-                item.vote_count || 0,
-
-            _isUnreleased:
-                releaseDate > today
-        }
-
-    })
-
-    const topRanked =
-        [...enriched]
-            .sort(
-                (a, b) =>
-                    b._vote - a._vote
-            )
-            .slice(0, 3)
-
-    const topIds =
-        new Set(
-            topRanked.map(
-                i => i.id
-            )
-        )
-
-    const unreleased =
-        enriched
-            .filter(
-                media =>
-                    media._isUnreleased &&
-                    !topIds.has(media.id)
-            )
-            .sort(
-                (a, b) =>
-                    b._vote - a._vote
-            )
-
-    const others =
-        enriched
-            .filter(
-                media =>
-                    !media._isUnreleased &&
-                    !topIds.has(media.id)
-            )
-            .sort(
-                (a, b) =>
-                    b._vote - a._vote
-            )
-
     if (!userId) {
-        if (onlyLiquid) {
-            return others
+        const today = Date.now()
+
+        const source = medias
+            .filter(filterMediaQuality)
+            .filter(
+                media =>
+                    !onlyLiquid || isLiquidMedia(media, userId)
+            )
+
+        const enriched = source.map(item => {
+
+            const releaseDate = new Date(
+                item.release_date ||
+                item.first_air_date ||
+                0
+            ).getTime()
+
+            return {
+
+                ...item,
+
+                _vote:
+                    item.vote_count || 0,
+
+                _isUnreleased:
+                    releaseDate > today
+            }
+
+        })
+
+        const topRanked =
+            [...enriched]
+                .sort(
+                    (a, b) =>
+                        b._vote - a._vote
+                )
+                .slice(0, 3)
+
+        const topIds =
+            new Set(
+                topRanked.map(
+                    i => i.id
+                )
+            )
+
+        const unreleased =
+            enriched
+                .filter(
+                    media =>
+                        media._isUnreleased &&
+                        !topIds.has(media.id)
+                )
+                .sort(
+                    (a, b) =>
+                        b._vote - a._vote
+                )
+
+        const others =
+            enriched
+                .filter(
+                    media =>
+                        !media._isUnreleased &&
+                        !topIds.has(media.id)
+                )
+                .sort(
+                    (a, b) =>
+                        b._vote - a._vote
+                )
+
+        if (!userId) {
+            if (onlyLiquid) {
+                return others
+            }
         }
     }
 
