62a9a6bfba0e1816061da3b27d657a9e4404876b
vlad3eko <vlad3eko@gmail.com>
2026-07-12 13:11:38 +0300

    edit server bot refactor architecture

--- server/bot/handlers/media/chosenInlineMedia.ts
+++ server/bot/handlers/media/chosenInlineMedia.ts
@@ -13,7 +13,7 @@ export const chosenInlineMedia = async (ctx: any) => {
 
         if (!inlineMessageId) return
 
-        const [mediaType, mediaId] = result.result_id.split('_')
+        const [_, mediaType, mediaId] = result.result_id.split('_')
         console.log('mediaType, mediaId', mediaType, mediaId)
 
         const media = await $fetch(
