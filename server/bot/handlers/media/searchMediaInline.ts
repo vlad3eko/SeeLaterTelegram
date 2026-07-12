d5c3671d3489018dde4ded07e1bcc07c8a48bc57
vlad3eko <vlad3eko@gmail.com>
2026-07-12 16:08:20 +0300

    edit server bot refactor favorites

--- server/bot/handlers/media/searchMediaInline.ts
+++ server/bot/handlers/media/searchMediaInline.ts
@@ -8,7 +8,7 @@ export const searchMediaInline = async (ctx: any) => {
 
         const page = Number(ctx.inlineQuery.offset) || 1
 
-        const medias = await searchMedia(ctx.inlineQuery.query, page)
+        const medias = await searchMedia(ctx.inlineQuery.query, page, ctx.from.id)
 
         if (!medias.results?.length) return ctx.answerInlineQuery([])
         await processSearchMediaInline(ctx, medias)
