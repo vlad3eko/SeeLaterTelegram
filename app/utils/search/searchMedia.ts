b1e3b4c1085cfae016774c8d84124d73d9ca3261
vlad3eko <vlad3eko@gmail.com>
2026-07-12 17:32:59 +0300

    edit server bot refactor favorites

--- app/utils/search/searchMedia.ts
+++ app/utils/search/searchMedia.ts
@@ -24,8 +24,8 @@ export const searchMedia = async (query: string, page: number = 1, userId: numbe
 
     const medias = result.results
         .map(normalizeTmdbMedia)
-        // .filter(filterTmdbMediaResults)
-        // .map(normalizeMediaGenres)
+        .filter(filterTmdbMediaResults)
+        .map(normalizeMediaGenres)
     console.log('medias filters', medias)
 
     if (page === 1) {
