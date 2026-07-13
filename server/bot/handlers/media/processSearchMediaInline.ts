1c2da0bc96519a902d27ef7f1c068686f865f12d
vlad3eko <vlad3eko@gmail.com>
2026-07-13 14:51:44 +0300

    edit server bot refactor favorites

--- server/bot/handlers/media/processSearchMediaInline.ts
+++ server/bot/handlers/media/processSearchMediaInline.ts
@@ -3,7 +3,7 @@ import {keyboardSendMediaCardInline} from "#server/bot/consts/buttons/keyboardBo
 
 export const processSearchMediaInline = async (ctx: any, medias: any) => {
 
-    console.log('10 processSearchMediaInline', medias)
+    console.log('10 processSearchMediaInline', medias[0])
     try {
 
         const results = medias.results.map(
