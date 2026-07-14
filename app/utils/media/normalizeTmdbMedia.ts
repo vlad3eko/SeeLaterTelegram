267b4310b5d32683ca505f664e853089784081de
vlad3eko <vlad3eko@gmail.com>
2026-07-14 15:25:06 +0300

    edit server bot refactor favorites

--- app/utils/media/normalizeTmdbMedia.ts
+++ app/utils/media/normalizeTmdbMedia.ts
@@ -16,8 +16,8 @@ export const normalizeTmdbMedia = (media: any) => {
         ...media,
         id: (media.tmdb_id || media.id),
         media_type: mediaType,
-        vote_average: FormatRating(media.vote_average),
-        vote_count: `${media.vote_count ? '🍿' + media.vote_count : ''}`,
+        vote_average: '💎' + FormatRating(media.vote_average),
+        vote_count: '🍿' + media.vote_count,
         title:
             media.title ||
             media.name,
