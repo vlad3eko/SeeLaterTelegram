cf63e2a88342820d959774c6f4c391697702b6ec
vlad3eko <vlad3eko@gmail.com>
2026-07-15 18:49:36 +0300

    edit server bot refactor favorites

--- server/api/bot/search/saveSearchQuery.post.ts
+++ server/api/bot/search/saveSearchQuery.post.ts
@@ -1,7 +1,24 @@
+import {serverSupabaseClient} from "#supabase/server";
+
 export default defineEventHandler(async (event) => {
     const query = getQuery(event)
 
     console.log('query q', query.q)
     console.log('query media_type', query.media_type)
     console.log('query user_id', query.user_id)
+
+    const supabase = await serverSupabaseClient(event)
+
+    const searchQuery = []
+    const mediaType = query.media_type === 'movie' ? 'фильм' : 'сериал'
+    const filters = searchQuery.push(query.q, mediaType)
+    console.log('filters', filters)
+    const telegramId = query.user_id
+
+    await supabase
+        .from('last_inline_search')
+        .upsert({
+            telegram_id: telegramId,
+            last_search_query: searchQuery
+        })
 })
