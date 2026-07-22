6df489fabd7a3792d86813fd3ad0b89195c98b1a
vlad3eko <vlad3eko@gmail.com>
2026-07-22 21:34:17 +0300

    edit server bot adminButtons

--- server/bot/handlers/media/searchMediaInline.ts
+++ server/bot/handlers/media/searchMediaInline.ts
@@ -26,6 +26,7 @@ export const searchMediaInline = async (ctx: any) => {
         const page = Number(ctx.inlineQuery.offset) || 1
         const medias = await searchMedia(ctx.inlineQuery.query, page, ctx.from.id)
 
+        console.log('medias.results', medias.results[0])
         if (!medias.results?.length) {
 
             const isCollection =
