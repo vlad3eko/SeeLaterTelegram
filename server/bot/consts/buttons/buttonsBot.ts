64d648d4bbca167e41e69bb5112a1bdaac1fd934
vlad3eko <vlad3eko@gmail.com>
2026-07-25 14:51:45 +0300

    edit server bot saveCount

--- server/bot/consts/buttons/buttonsBot.ts
+++ server/bot/consts/buttons/buttonsBot.ts
@@ -61,7 +61,7 @@ export const SearchButtonBot = (
 export const SaveMediaButtonBot = (mediaId: number, mediaType: string, contentType?: string | undefined) => {
     let count = 0
     return Markup.button.callback(
-        `💾 Сохранить(${++count})`,
+        `💾 Сохранить(${count++})`,
         `save_media_${mediaId}_${mediaType}_${contentType}`)
 }
 
