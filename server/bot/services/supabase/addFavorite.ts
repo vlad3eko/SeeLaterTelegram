adf6af8735159bf4059a42db2dfa90284ef0aab1
vlad3eko <vlad3eko@gmail.com>
2026-07-25 11:42:41 +0300

    edit server bot adminButtons

--- server/bot/services/supabase/addFavorite.ts
+++ server/bot/services/supabase/addFavorite.ts
@@ -5,14 +5,18 @@ export async function createFavorite(
     return await supabase
         .from('favorites')
         .insert({
-            user_id: payload.userId,
-            tmdb_id: payload.tmdbId,
-            title: payload.title,
-            media_type: payload.mediaType,
-            poster_path: payload.posterPath,
-            vote_average: payload.voteAverage,
-            vote_count: payload.voteCount,
-            release_date: payload.releaseDate,
-        })
+                user_id: payload.userId,
+                tmdb_id: payload.tmdbId,
+                title: payload.title,
+                media_type: payload.mediaType,
+                poster_path: payload.posterPath,
+                vote_average: payload.voteAverage,
+                vote_count: payload.voteCount,
+                release_date: payload.releaseDate,
+            },
+            {
+                onConflict: 'user_id,tmdb_id',
+                ignoreDuplicates: true
+            })
         .select()
 }
