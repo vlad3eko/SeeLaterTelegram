b26ff09def852b1895b3c43717df42c56966710a
vlad3eko <vlad3eko@gmail.com>
2026-08-22 14:35:18 +0300

    push update person

--- server/api/tmdb/credits.get.ts
+++ server/api/tmdb/credits.get.ts
@@ -99,41 +99,5 @@ export default defineEventHandler(async (event) => {
         )
     }
 
-    /*
-  * =========================
-  * FILTER
-  * =========================
-  */
-
-    const filteredResults = results.filter(
-        (media: any) =>
-            filterTmdbMediaResults(media)
-    )
-
-    /*
-     * =========================
-     * PAGINATION
-     * =========================
-     */
-
-    const totalResults = filteredResults.length
-
-    const totalPages = Math.ceil(
-        totalResults / PAGE_SIZE
-    )
-
-    const start = (page - 1) * PAGE_SIZE
-    const end = start + PAGE_SIZE
-
-    const paginatedResults = filteredResults.slice(
-        start,
-        end
-    )
-
-    return {
-        page,
-        results: paginatedResults,
-        total_pages: totalPages,
-        total_results: totalResults
-    }
+    return results
 })
