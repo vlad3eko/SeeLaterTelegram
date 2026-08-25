897e8880d8fb06025bfc697544c2859f5cb0c6a6
vlad3eko <vlad3eko@gmail.com>
2026-08-26 00:49:23 +0300

    push update person api

--- server/global/engine/search/person/personSearch.ts
+++ server/global/engine/search/person/personSearch.ts
@@ -3,7 +3,13 @@ import {normalizeTmdbMedia} from "~/utils/media/normalizeTmdbMedia";
 export const personSearch = (result: any, cacheOptions: any) => {
     result.results = (result.results || [])
         .map(normalizeTmdbMedia)
-        .filter((person: any) => person.media_type === 'person')
+        .filter((person: any) =>
+            person.media_type === "person"
+            || person.known_for_department
+            || person.character
+            || person.job
+            || person.credit_id
+        )
         .filter((person: any) => Boolean(person.profile_path))
         .sort((a: any, b: any) => (b.popularity || 0) - (a.popularity || 0))
 
