266ee51fc1c16d85b0eea7ddbdee07d0de87df29
vlad3eko <vlad3eko@gmail.com>
2026-09-16 20:18:50 +0300

    rich cards

--- server/bot/actions/admin/editAdminInlineMedia.ts
+++ server/bot/actions/admin/editAdminInlineMedia.ts
@@ -3,6 +3,7 @@ import {tmdbFetch} from "#server/utils/api/tmdbFetch"
 import {editMediaChoiceKeyboard} from "#server/bot/consts/buttons/admin/keyboardAdmin";
 import {NOTIFICATION_MESSAGE} from "#server/global/notifications/sendNotificationMessage";
 import {engineRichCard} from "#server/global/engine/card/engineRichCard";
+import {getTelegramMediaImages} from "#server/global/engine/card/construct/getTelegramPosterFileId";
 
 export const editAdminInlineMedia = async (ctx: any) => {
 
@@ -47,6 +48,20 @@ export const editAdminInlineMedia = async (ctx: any) => {
                 }
             )
 
+        const telegramImages =
+            await getTelegramMediaImages(
+                ctx,
+                parsedMediaId,
+                mediaType,
+                {
+                    poster: [
+                        media.poster_path ||
+                        media.backdrop_path
+                    ],
+                    postersList: []
+                }
+            )
+
         setAdminEditSession(
             ctx.from.id,
             {
@@ -61,11 +76,7 @@ export const editAdminInlineMedia = async (ctx: any) => {
                 mode: undefined,
                 currentMedia: {
                     type: "photo",
-                    fileId:
-                        `https://image.tmdb.org/t/p/original${
-                            media.poster_path ||
-                            media.backdrop_path
-                        }`
+                    fileId: telegramImages.poster[0]!
                 }
             }
         )
