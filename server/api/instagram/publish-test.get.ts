6971d005b1d909ab5b56fb893a3d490696bdd117
vlad3eko <vlad3eko@gmail.com>
2026-09-17 23:05:05 +0300

    test api inst

--- server/api/instagram/publish-test.get.ts
+++ server/api/instagram/publish-test.get.ts
@@ -1,21 +1,24 @@
 export default defineEventHandler(async () => {
-
     const token = process.env.INSTAGRAM_ACCESS_TOKEN
     const accountId = process.env.INSTAGRAM_ACCOUNT_ID
 
-    if (!token || accountId) {
+    if (!token || !accountId) {
         throw createError({
             statusCode: 500,
             statusMessage: 'Instagram environment variables are missing'
         })
     }
 
-    const imageUrl = 'https://www.themoviedb.org/t/p/w600_and_h900_face/pK8CH9JxrgX2ZIq3WclTwnX0cCL.jpg'
-    const caption = 'Тестовая публикация Kinomanovnet'
-    const container = await $fetch<{id: string}>(
+    const imageUrl =
+        'https://image.tmdb.org/t/p/w1080/nuNOKDt0mVWhwq2HZ9ytczuUFSf.jpg'
+
+    const caption =
+        'Тестовая публикация KinomanovNet'
+
+    const container = await $fetch<{ id: string }>(
         `https://graph.instagram.com/v26.0/${accountId}/media`,
         {
-            method: "POST",
+            method: 'POST',
             headers: {
                 Authorization: `Bearer ${token}`
             },
@@ -26,10 +29,10 @@ export default defineEventHandler(async () => {
         }
     )
 
-    const published = await $fetch<{id: string}>(
+    const published = await $fetch<{ id: string }>(
         `https://graph.instagram.com/v26.0/${accountId}/media_publish`,
         {
-            method: "POST",
+            method: 'POST',
             headers: {
                 Authorization: `Bearer ${token}`
             },
